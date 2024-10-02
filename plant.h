bef8b393c20010a26382ada9eea6961f643f00f3
champ <2681158691@qq.com>
2024-10-02 17:31:04 +0800

    Update plant.h

--- plant.h
+++ plant.h
@@ -8,15 +8,15 @@
 
 class Plant : public QGraphicsItem
 {
-public:
+public://便于与zombie交互
     int hp;
     int state;
-    enum { Type = UserType + 1};
+    enum { Type = UserType + 1};//？
     Plant();
     ~Plant() override;
-    QRectF boundingRect() const override;
+    QRectF boundingRect() const override;//返回占用面积
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
-    bool collidesWithItem(const QGraphicsItem *other, Qt::ItemSelectionMode mode) const override;
+    bool collidesWithItem(const QGraphicsItem *other, Qt::ItemSelectionMode mode) const override;//判断碰撞
     int type() const override;
     void setMovie(QString path);
 protected:
