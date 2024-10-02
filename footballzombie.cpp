ce4e1c82cf2d43c83cd0e00c04b63760a366f51d
champ <2681158691@qq.com>
2024-10-02 17:48:42 +0800

    Update footballzombie.cpp

--- footballzombie.cpp
+++ footballzombie.cpp
@@ -10,12 +10,12 @@ FootballZombie::FootballZombie()
 
 void FootballZombie::advance(int phase)
 {
-    if (!phase)
+    if (!phase)//????????
         return;
     update();
     if (hp <= 0)
     {
-        if (state < 2)//未死亡
+        if (state < 2)//state0 -> 行走  state1->攻击   state2->死亡
         {
             state = 2;//判定死亡
             setMovie(":/images/FootballZombieDie.gif");
@@ -26,19 +26,19 @@ void FootballZombie::advance(int phase)
             delete this;
         return;
     }
-    QList<QGraphicsItem *> items = collidingItems();//QGraphics中重叠的items
+    QList<QGraphicsItem *> items = collidingItems();//QGraphics中与footballzombie碰撞的items
     if (!items.isEmpty())
     {
-        Plant *plant = qgraphicsitem_cast<Plant *>(items[0]);
-        plant->hp -= atk;
-        if (state != 1)
+        Plant *plant = qgraphicsitem_cast<Plant *>(items[0]);//第一个与zombie碰撞的plant，转换成plant指针以修改plant_hp，如果碰撞的不是plant，该指针指向nullptr
+        if(plant) plant->hp -= atk;
+        if (state != 1)//state==0 
         {
             state = 1;
-            setMovie(":/images/FootballZombieAttack.gif");
+            setMovie(":/images/FootballZombieAttack.gif");//前movie指针存在，删掉并修改成播放攻击gif的指针
         }
         return;
     }
-    if (state)
+    if (state) //state==1
     {
         state = 0;
         setMovie(":/images/FootballZombieWalk.gif");
